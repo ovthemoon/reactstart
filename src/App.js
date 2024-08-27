07ab865d1acbc9327885ae5ff63ca2e2bdf06659
ovthemoon <seungmoonchoi71@gmail.com>
2024-08-27 15:18:36 +0900

    Add : Homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// 필요한 모듈과 컴포넌트를 가져옵니다.
+import Button from "./Button"
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange=(e)=>setTodo(e.target.value);
+  console.log(todo);
+  const onSubmit=(e)=>{e.preventDefault();
+    if(todo===""){
+      alert("할 일을 입력하세요");
+      return;
+    }
+    setTodo("");
+    setTodos(currentArray=>[todo,...currentArray]);
+  };
+  
+  return <div>
+    <h1>todos length{todos.length}</h1>
+    <form>
+      <input onChange={onChange} value={todo} type="text" placeholder="할 일을 입력하세요" />
+      <button onClick={onSubmit}>Add ToDo</button>
+    </form>
+    <hr />
+    {todos.map((todo,index)=><li key={index}>{todo}</li>)}
+  </div>
 }
 
 export default App;
