9eaa185a97681087a1dc363b5b21263606d998d7
ovthemoon <seungmoonchoi71@gmail.com>
2024-08-27 15:27:01 +0900

    Add : Components

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,22 +1,20 @@
 import Home from "./routes/home";
 import Detail from "./routes/Detail";
-//Hash Router/ Browser Router
 import {
     BrowserRouter as Router,
-    Switch,
+    Routes,
     Route,
 } from "react-router-dom";
+
 function MovieApp() {
-    return <Router>
-        <Switch>
-            <Route path="/movie/:id">
-                <Detail />
-            </Route>
-            <Route path="/">
-                <Home />
-            </Route>
-        </Switch>
-    </Router>
+    return (
+        <Router>
+            <Routes>
+                <Route path="/movie/:id" element={<Detail />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default MovieApp;
